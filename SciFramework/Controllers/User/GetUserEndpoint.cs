c85ec7eca5ba6eb9572c19309748e96d62308d5d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-03-13 18:29:41 +0100

    refactor(UserEndpoint): removing redundant using directives

--- SciFramework/Controllers/User/GetUserEndpoint.cs
+++ SciFramework/Controllers/User/GetUserEndpoint.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SciFramework.Controllers.User
+﻿namespace SciFramework.Controllers.User
 {
     public partial class UserEndpoint
     {
