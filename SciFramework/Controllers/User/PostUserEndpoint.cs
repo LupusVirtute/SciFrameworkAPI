5ae2fe24823f88f6c5c5a5c58b8e6632af95838d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-03-15 17:04:06 +0100

    feat(UserEndpoint): create unimplemented method

--- SciFramework/Controllers/User/PostUserEndpoint.cs
+++ SciFramework/Controllers/User/PostUserEndpoint.cs
@@ -1,6 +1,48 @@
-﻿namespace SciFramework.Controllers.User
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SciFramework.Controllers.User
 {
     public partial class UserEndpoint
     {
+        [HttpPost("login")]
+        public ActionResult<string> Login(string email, string password)
+        {
+            string authKey;
+
+            throw new NotImplementedException();
+
+            return authKey;
+        }
+
+        [HttpPost("register")]
+        public ActionResult<bool> Register(string email)
+        {
+            bool isEmailAlreadyRegistered;
+
+            throw new NotImplementedException();
+
+            return isEmailAlreadyRegistered;
+        }
+
+        [HttpPost("resetPassword")]
+        public ActionResult<bool> ResetPassword(string email)
+        {
+            bool isEmailRegistered;
+
+            throw new NotImplementedException();
+
+            return isEmailRegistered;
+        }
+
+        [HttpPost("profileImage")]
+        public ActionResult<bool> ProfileImage(string profileImage, string authKey)
+        {
+            bool isSuccesed;
+
+            throw new NotImplementedException();
+
+            return isSuccesed;
+        }
     }
 }
