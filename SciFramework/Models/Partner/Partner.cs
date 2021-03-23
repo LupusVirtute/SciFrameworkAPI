035285acb2e5c9974822bf89506891be31251044
Dawid Czyżewski <dawidek12.04@wp.pl>
2021-03-23 19:03:14 +0100

    refactor: Partner class

--- SciFramework/Models/Partner/Partner.cs
+++ SciFramework/Models/Partner/Partner.cs
@@ -1,32 +1,28 @@
-﻿using System;
+﻿using SciFramework.Interfaces.Partner;
+using System;
+using SciFramework.Abstract.Database;
 
 namespace SciFramework.Models.Partner
 {
-	public class Partner : Saveable
+	public class Partner : Saveable,IPartner
 	{
 
 		private string _name;
 		private string _description;
+        private string _b64PartnerIcon;
 
+		public string Name => _name;
+		public string Description => _description;
+        public string B64Image => _b64PartnerIcon;
 
-
-		public string name
-		{
-			get => _name;
-			set => _name = value;
-		}
-
-		public string description
-        {
-			get => _description;
-			set => _description = value;
-        }
-
-		public Partner(string name,string description)
+		public Partner(string name,string description,string partnerIcon)
 		{
 			this._name = name;
 			this._description = description;
+            this._partnerIcon = partnerIcon;
 		}
+
+
 
 		
 	}
