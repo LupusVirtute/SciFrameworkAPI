eede9f48ab525b845790a73d9b1a67148462e71f
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-03-25 21:34:11 +0100

    test(UserEndpointTests.cs): add UserEndpoint property

--- UnitTests/Controllers/User/UserEndpointTests.cs
+++ UnitTests/Controllers/User/UserEndpointTests.cs
@@ -1,6 +1,14 @@
-﻿namespace UnitTests.Controllers.User
+﻿using SciFramework.Controllers.User;
+
+namespace UnitTests.Controllers.User
 {
     public partial class UserEndpointTests
     {
+        UserEndpoint userEndpoint;
+
+        public UserEndpointTests()
+        {
+            userEndpoint = new TestUserEndpoint();
+        }
     }
 }
