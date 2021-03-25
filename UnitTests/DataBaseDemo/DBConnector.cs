fb59acb0799e926734ea21a1388bb4232cbf1949
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-03-25 21:24:57 +0100

    feat(DBConnector.cs): add class generate DbConnection to DemoDB.db

---
+++ UnitTests/DataBaseDemo/DBConnector.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SQLite;
+using Dapper;
+using Xunit;
+
+namespace UnitTests.DataBaseDemo
+{
+    public class DBConnector
+    {
+        const string CONNECTION_STR = "Data Source=./DataBaseDemo/DemoDB.db;Version=3";
+
+        public static IDbConnection DbConnection
+        {
+            get => new SQLiteConnection(CONNECTION_STR);
+        }
+
+        [Fact]
+        public void testConnectToDemoDB()
+        {
+            using (IDbConnection connection = DbConnection)
+            {
+                var output = connection.Query<object>($"select * from TestTable", new DynamicParameters());
+            }
+        }
+    }
+}
